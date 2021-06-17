3ba18478cc49a6305dd3adce77e36a2dc8811072
Sebastian Nanek <snanek@gmail.com>
2021-06-17 08:56:30 +0000

    content_keywords

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
     def content_snapshot
         content.split('.').first.strip
     end
+
+    def content_keywords(count)
+        content.split('.').map {|x|x.split(' ')}.flatten.sample(count)
+    end
 end
--- spec/model/post_spec.rb
+++ spec/model/post_spec.rb
@@ -14,4 +14,14 @@ describe Post do
             expect(post.content_snapshot).to eq("Sed ut dolor u nde omnis iste natues")
         end
     end
+
+    describe '#content_keywords' do
+        let(:count) { 6 }
+
+        it 'returns an array of _count_ words' do
+            post = Post.new(content: my_content)
+            
+            expect(post.content_keywords(count).length).to eq(count)
+        end
+    end
 end
